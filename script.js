7e4b9c5e9bb571c30959b83c4252d8e72f79b931
Kiran <kiran0503@gmail.com>
2022-04-30 22:14:31 +0530

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Image should come back to original position when zoomed in at one point and zoomed out at some other point
-
-
 const canvas = new fabric.Canvas('canvas', {
     width: 900,
     height: 450,
@@ -8,19 +5,16 @@ const canvas = new fabric.Canvas('canvas', {
 });
 
 
-const img = document.getElementById('img');
 const reader = new FileReader();
 const inputFile = document.getElementById('files');
 const clearBtn = document.getElementById('clear');
 const downloadBtn = document.getElementById('download');
-let imgF = null;
-
-
+let image;
 
 
 const imgAdded = (e) => {
     const inputElement = document.getElementById('files');
-    const file = inputElement.files[0];    
+    const file = inputElement.files[0];
 
     reader.readAsDataURL(file);
 }
@@ -33,12 +27,10 @@ reader.addEventListener('load', () => {
     fabric.Image.fromURL(reader.result, img => {
         canvas.add(img);
         canvas.centerObject(img);
+        image = img;
         canvas.requestRenderAll();
-
-
-        imgMove(canvas, img);
-    })  
-})
+    });
+});
 
 
 
@@ -46,46 +38,44 @@ reader.addEventListener('load', () => {
 clearBtn.addEventListener('click', () => {
     canvas.getObjects().forEach((obj) => {
         canvas.remove(obj);
-    })
+    });
 })
 
 
 
-const imgMove = (canvas, img) => {
-    canvas.on('mouse:wheel', (opt) => {
-        let delta = opt.e.deltaY;
-        let zoom = canvas.getZoom();
-        zoom *= 0.999 ** delta;
-    
-        if(zoom > 20){
-            zoom = 20;
-        }
-    
-        if(zoom < 0.5) {
-            
-            canvas.centerObject(img);
-            canvas.requestRenderAll();
-            zoom = 1;
-            
-        }
-    
-        
-    
-        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-        opt.e.preventDefault();
-        opt.e.stopPropagation();
-    
-    })
-}
-
 
 downloadBtn.addEventListener('click', () => {
-    
-    
+
     const a = document.createElement('a');
     a.href = document.getElementById('canvas').toDataURL("image/png");
     a.download = "image.png";
     a.click();
 
+});
 
-})
+
+
+canvas.on('mouse:wheel', function (opt) {
+    let delta = opt.e.deltaY;
+    let zoom = canvas.getZoom();
+    zoom *= 0.999 ** delta;
+
+    if (zoom > 20) {
+        zoom = 20;
+        this.lastPosX = opt.e.clientX;
+        this.lastPosY = opt.e.clientY;
+    }
+
+    if (zoom < 0.5 && (this.lastPosX !== opt.e.clientX && this.lastPosY !== opt.e.clientY)) {
+        zoom = 1;
+        this.clear();
+        this.add(image);
+        this.centerObject(image);
+        this.requestRenderAll();
+    }
+
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+
+});
